d3f26f610e7c975b74b230cc884b5f3cefef2f30
JasperHard0128 <80259215+JasperHard0128@users.noreply.github.com>
2021-03-08 20:05:36 +0800

    Add files via upload

---
+++ readdata.py
@@ -0,0 +1,8 @@
+import torch
+from torch.utils.tensorboard import SummaryWriter
+writer = SummaryWriter("logs")
+
+writer.add_image()
+for i in range(100):
+    writer.add_scalar("y =2x", 3*i, i)
+writer.close()
